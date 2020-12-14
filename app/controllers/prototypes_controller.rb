c0cebc69e0e84aee1c568ac107f2da060c49979a
ynghr1226 <74960629+ynghr1226@users.noreply.github.com>
2020-12-14 14:43:53 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-    before_action :move_to_index, only: [:edit] 
+    # before_action :move_to_index, only: [:edit] 
     before_action :authenticate_user!, except:[:index,:show]
 
     def index
